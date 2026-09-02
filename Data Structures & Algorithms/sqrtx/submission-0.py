2acd6d27a0037b3778f7e4f4007246824c359556
fagun_chauhan <chauhan2004fagun@gmail.com>
2026-09-02 16:10:06 +0530

    Update: sqrtx - submission-0

--- Data Structures & Algorithms/sqrtx/submission-0.py
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -1,10 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-            res=0
-        for i in range(1, x+1):
-            if i*i > x:
-                return res
-            res=i
+        l=0
+        r=x
+        res=0
+        while l<=r:
+            mid = l + ((r-l)//2)
+            if mid * mid > x:
+                r = mid -1
+            elif mid * mid <x:
+                l = mid +1
+                res = mid
+            else:
+                return mid
         return res
